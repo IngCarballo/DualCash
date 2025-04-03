92789d5846e55dfbd07fd0ef89c318ac52fcabc0
nicolasCarballo <nicolas.carballo@um.edu.ar>
2025-04-03 15:37:45 -0300

    create the new models

--- dualcash_api/models.py
+++ dualcash_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    ]
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
